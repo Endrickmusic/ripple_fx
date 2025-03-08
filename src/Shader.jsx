c451be973548dd879becda7b9274eba5167eeed3
Christian Hohenbild <mail@christianhohenbild.de>
2025-03-08 02:03:06 +0100

    add displacment

--- src/Shader.jsx
+++ src/Shader.jsx
@@ -113,13 +113,23 @@ export default function Shader() {
         uniform sampler2D uTexture;
         uniform sampler2D uDisplacement;
         varying vec2 vUv;
+
+        float PI = 3.14159265359;
         
         void main() {
-          vec4 color = texture2D(uTexture, vUv);
-        //   vec4 color = texture2D(uDisplacement, vUv);
+
+          vec4 displacement = texture2D(uDisplacement, vUv);
+          float theta = displacement.r * 2. * PI;
+
+          vec2 dir = vec2(sin(theta), cos(theta));
+
+          vec2 uv = vUv + dir * displacement.r;
+          vec4 color = texture2D(uTexture, uv);
+         
           
           // Blend the render target with the cloud texture
           gl_FragColor = vec4(color);
+        //   gl_FragColor = vec4(displacement);
         }
       `,
       uniforms: {
